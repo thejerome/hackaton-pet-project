dd62b7e09cc41ef2c382c7661d3753cc83ca24d6
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-10-09 12:18:47 +0300

    Initial Commit

---
+++ src/main/java/com/example/hackatonpetproject/HackatonPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackatonpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonPetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackatonpetproject/HackatonPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackatonpetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
